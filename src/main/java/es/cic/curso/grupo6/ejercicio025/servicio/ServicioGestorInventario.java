fa908a958b30ddaa018a0b92a91d65762bd19f5a
curso17 <curso17java@gmail.com>
2017-02-20 12:20:06 +0100

    Subida rota (en estado de reparación)

--- src/main/java/es/cic/curso/grupo6/ejercicio025/servicio/ServicioGestorInventario.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio025/servicio/ServicioGestorInventario.java
@@ -1,5 +1,13 @@
 package es.cic.curso.grupo6.ejercicio025.servicio;
 
+import java.util.List;
+
+import es.cic.curso.grupo6.ejercicio025.modelo.Producto;
+
 public interface ServicioGestorInventario {
 
+	public List<Producto> obtenListaAlmacen();
+
+	public List<Producto> obtenListaTienda();
+	
 }
--- src/main/java/es/cic/curso/grupo6/ejercicio025/servicio/ServicioGestorInventarioImpl.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio025/servicio/ServicioGestorInventarioImpl.java
@@ -1,5 +1,20 @@
 package es.cic.curso.grupo6.ejercicio025.servicio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import es.cic.curso.grupo6.ejercicio025.modelo.Producto;
+
 public class ServicioGestorInventarioImpl implements ServicioGestorInventario {
+	
+	@Override
+	public List<Producto> obtenListaAlmacen() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public List<Producto> obtenListaTienda() {
+		return new ArrayList<>();
+	}
 
 }
