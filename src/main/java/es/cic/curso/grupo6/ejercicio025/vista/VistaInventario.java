bc32eff94ef64009b76205c2355adbced7e003c5
curso17 <curso17java@gmail.com>
2017-02-20 10:35:03 +0100

    Infraestructura de vistas

---
+++ src/main/java/es/cic/curso/grupo6/ejercicio025/vista/VistaInventario.java
@@ -0,0 +1,21 @@
+package es.cic.curso.grupo6.ejercicio025.vista;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
+
+public class VistaInventario extends VerticalLayout implements View {
+	private static final long serialVersionUID = 4023808594401737793L;
+
+	public VistaInventario(Navigator navegador) {
+		
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/es/cic/curso/grupo6/ejercicio025/vista/VistaTienda.java
@@ -0,0 +1,21 @@
+package es.cic.curso.grupo6.ejercicio025.vista;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.VerticalLayout;
+
+public class VistaTienda extends VerticalLayout implements View {
+	private static final long serialVersionUID = -4756028504271319024L;
+
+	public VistaTienda(Navigator navegador) {
+		
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
