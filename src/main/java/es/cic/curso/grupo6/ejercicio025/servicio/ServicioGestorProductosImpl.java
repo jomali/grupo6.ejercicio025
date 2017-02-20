5c87fba9f81a0a2a5790bf05fbb4f981ee89edc4
curso17 <curso17java@gmail.com>
2017-02-20 14:12:42 +0100

    Correcciones en la configuración

--- src/main/java/es/cic/curso/grupo6/ejercicio025/servicio/ServicioGestorProductosImpl.java
+++ src/main/java/es/cic/curso/grupo6/ejercicio025/servicio/ServicioGestorProductosImpl.java
@@ -1,5 +1,10 @@
 package es.cic.curso.grupo6.ejercicio025.servicio;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class ServicioGestorProductosImpl implements ServicioGestorProductos {
 
 }
