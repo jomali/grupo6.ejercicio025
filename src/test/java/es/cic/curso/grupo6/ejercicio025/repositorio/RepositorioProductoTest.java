f67b7dd03ff5fb1a2d15b2e7af9c8f2e92c1a6cd
curso17 <curso17java@gmail.com>
2017-02-20 13:24:51 +0100

    Creado directorio para tests

---
+++ src/test/java/es/cic/curso/grupo6/ejercicio025/repositorio/RepositorioProductoTest.java
@@ -0,0 +1,18 @@
+package es.cic.curso.grupo6.ejercicio025.repositorio;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RepositorioProductoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
